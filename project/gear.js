70d1eff6de776fd96a1b2547f2cff3c640e7acaa
Steven Earl Davis Jr <119690737+Edav08@users.noreply.github.com>
2023-03-12 22:26:06 -0400

    Add files via upload

---
+++ project/gear.js
@@ -0,0 +1,177 @@
+module.exports = [
+    {name:"guilty hood",
+    type:"armor",
+    armor: 1,
+    slot: "head",
+    rarity: "common"
+    },{
+    name:"cloth garb",
+    type:"armor",
+    armor: 1,
+    slot:"chest",
+    rarity: "common"
+    },{
+    name:"cloth trousers",
+    type:"armor",
+    armor: 1,
+    slot:"legs",
+    rarity:"common"
+    },{
+    name:"war surgeon gown",
+    type:"armor",
+    armor:2,
+    slot:"chest",
+    rarity:"common"
+    },{
+    name:"war surgeon trousers",
+    type:"armor",
+    armor:2,
+    slot:"legs",
+    rarity:"common"
+    },{
+    name:"white mask ",
+    type:"armor",
+    armor:2,
+    slot:"head",
+    rarity:"common"
+    },
+    {
+    name:"phophet robe",
+    type:"armor",
+    armor:3,
+    slot:"chest",
+    rarity:"common",
+    },{
+    name:"phophet trousers",
+    type:"armor",
+    armor:3,
+    slot:"legs",
+    rarity:"common"
+    },{
+    name:"phophet blindfold",
+    type:"armor",
+    armor:3,
+    slot:"head",
+    rarity:"common",
+    },{
+    name:"prisoner clothing",
+    type:"armor",
+    armor:4,
+    slot:"chest",
+    rarity:"common"
+    },{
+    name:"prisoner trousers",
+    type:"armor",
+    armor:4,
+    slot:"legs",
+    rarity:"common"
+    },{
+    name:"fire monk armor",
+    type:"armor",
+    armor:4,
+    slot:"chest",
+    rarity:"common"
+    },{
+    name:"fire monk greaves",
+    type:"armor",
+    armor:4,
+    slot:"legs",
+    rarity:"common"
+    },{
+    name:"fire monk hood",
+    type:"armor",
+    armor:4,
+    slot:"head",
+    rarity:"common"
+    },{
+    name:"bandit garb",
+    type:"armor",
+    armor: 5,
+    slot:"chest",
+    rarity:"common"
+    },{
+    name:"bandit boots",
+    type:"armor",
+    armor:5,
+    slot:"legs",
+    rarity:"common"
+    },{
+    name:"vagabond knight helm",
+    type:"armor",
+    armor: 5,
+    slot:"head",
+    rarity:"uncommon"
+    },{
+    name:"vagabond knight armor",
+    type:"armor",
+    armor:7,
+    slot:"chest",
+    rarity:"uncommon"
+    },{
+    name:"vagabond knight greaves",
+    type:"armor",
+    armor:6,
+    slot:"legs",
+    rarity:"uncommon"
+    },{
+    name:"twinned helm",
+    type:"armor",
+    slot:"head",
+    armor: 6,
+    rarity:"uncommon"
+    },{
+    name:"twinned armor",
+    type:"armor",
+    armor:7,
+    slot:"chest",
+    rarity:"uncommon"
+    },{
+    name:"twinned greaves",
+    type:"armor",
+    armor:6,
+    slot:"legs",
+    rarity:"uncommon"
+    },{
+    name:"sage hood",
+    type:"armor",
+    armor:7,
+    slot:"head",
+    rarity:"uncommon"
+    },{
+    name:"cruicible axe armor",
+    type:"armor",
+    armor: 9,
+    slot:"chest",
+    rarity:"rare"
+    },{
+    name:"cruicible greaves",
+    type:"armor",
+    armor: 8,
+    slot:"legs",
+    rarity:"rare"
+    },{
+    name:"cruicible axe helm",
+    type:"armor",
+    armor: 8,
+    slot:"head",
+    rarity:"rare" 
+    },{
+    name:"royal remains armor",
+    type:"armor",
+    armor: 12,
+    slot:"chest",
+    rarity:"legendary" 
+    },{
+    name:"royal remains greaves",
+    type:"armor",
+    armor: 12,
+    slot:"legs",
+    rarity:"legendary" 
+    },{
+    name:"royal remains helm",
+    type:"armor",
+    armor: 12,
+    slot:"head",
+    rarity:"legendary" 
+    }
+]
---
+++ project/weapon.js
@@ -0,0 +1,82 @@
+module.exports = [
+    {name:"claymore",
+    type: "weapon",
+    power: 1,
+    rarity:"common"
+    },{
+    name:"club",
+    type: "weapon",
+    power:3,
+    rarity:"common"
+    },{
+    name:"whip",
+    type: "weapon",
+    power:4,
+    rarity:"common"
+    },{
+    name:"great sword",
+    type: "weapon",
+    power:4,
+    rarity:"common"
+    },{
+    name:"ghiza's wheel",
+    type: "weapon",
+    power:5,
+    rarity:"common"
+    },{
+    name:"enoy's greathorn",
+    type: "weapon",
+    power:6,
+    rarity:"uncommon",
+    },{
+    name:"hand of malenia",
+    type: "weapon",
+    power:6,
+    rarity:"uncommon"
+    },{
+    name:"grafted blade great sword",
+    type: "weapon",
+    power:7,
+    rarity:"uncommon"   
+    },{
+    name:"morgott cursed sword",
+    type: "weapon",
+    power:7,
+    rarity:"uncommon" 
+    },    {
+    name:"marais executioner's sword",
+    type: "weapon",
+    power: 8,
+    rarity:"uncommon"
+    },{
+    name:"ruins greatsword",
+    type:"weapon",
+    power: 8,
+    rarity:"uncommon"
+    },{
+    name:"rivers of blood",
+    type: "weapon",
+    power: 16,
+    rarity: "rare"
+    },{
+    name:"moonveil",
+    type:"weapon",
+    power:16,
+    rarity: "rare"
+    },{
+    name:"lusat glintstone staff",
+    type:"weapon",
+    power:18,
+    rarity:"rare"
+    },{
+    name:"dark moon greatsword",
+    type:"weapon",
+    power:20,
+    rarity:"legendary"    
+    },{
+    name:"blasphemous blade",
+    type:"weapon",
+    power:20,
+    rarity:"legendary" 
+    }
+]
