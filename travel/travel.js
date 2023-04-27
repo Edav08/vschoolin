5e5978e9e3dba8c769fb9f1ebb4d6a18b910640d
Steven Earl Davis Jr <119690737+Edav08@users.noreply.github.com>
2023-04-26 20:49:06 -0400

    Add files via upload

---
+++ travel/travel.js
@@ -0,0 +1,18 @@
+var form = document.querySelector("#input-form");
+// var button = document.querySelector("#submit-button");
+
+form.addEventListener("submit", function(e){
+    alert("First Name: " + this.fname.value + 
+    "\nLast Name: " + this.lname.value + 
+    "\nAge: " + this.age.value +
+    "\nGender: " + this.gender.value +
+    "\nDestination: " + this.destination.value 
+    // "\nDiet: " + this.checkbox.checked
+
+    
+    
+    
+    
+    )
+    
+})
